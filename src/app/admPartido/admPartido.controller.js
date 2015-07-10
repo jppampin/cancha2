49603b7cdad69f48d682978cd0946c17d1c7478b
jpp <jppampin@gmail.com>
2015-07-10 02:15:59 -0300

    - Adding new facebook / check images - Adding new list behavior - Adding new login facebook button

---
+++ src/app/admPartido/admPartido.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cancha2')
+    .controller('AdmPartidoController', AdmPartidoController);
+
+  /** @ngInject */
+  function AdmPartidoController($timeout, webDevTec, toastr) {
+    var vm = this;
+          
+  }
+})();
--- src/app/components/list/list.directive.js
+++ src/app/components/list/list.directive.js
@@ -19,15 +19,18 @@
     return directive;
 
     /** @ngInject */
-    function CanchaListController() {
+    function CanchaListController(toastr) {
       var vm = this;
 
       vm.players = [
         { 'name' : 'JPP', 'email' : 'jppampin@gmail.com', 'comment': 'First User'},
         { 'name' : 'JPP2', 'email' : 'jppampin@gmail.com', 'comment': 'Second User'},
-        { 'name' : 'JPP3', 'email' : 'jppampin@gmail.com', 'comment': 'Third User'},
-        { 'name' : 'JPP4', 'email' : 'jppampin@gmail.com', 'comment': 'Fourth User'}
+        { 'name' : 'JPP3', 'email' : 'jppampin@gmail.com', 'comment': 'Third User', 'confirm' : true},
+        { 'name' : 'JPP4', 'email' : 'jppampin@gmail.com', 'comment': 'Fourth User', 'confirm': true}
       ]
+      vm.update = function(player){
+        toastr.info('Player ' + player.name + ' UPDATED!' );
+      }
     }
   }
 
