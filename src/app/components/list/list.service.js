72745e57c7fefb06a344bb6dbce74131936f7a04
unknown <pj011348@190103x.ciudad.bco>
2015-07-30 10:45:28 -0300

    - Adding add me backend - Adding underscore library - Adding ripple effecto to the cards - Changin label Username by Email - Removing commented elements. - Fixing backend proxy - Adding new model adjustments to the list - Adding adjustments to the addPlayer

--- src/app/components/list/list.service.js
+++ src/app/components/list/list.service.js
@@ -6,23 +6,48 @@
     .factory('listService', listService);
 
   /** @ngInject */
-  function listService($log, $http) {
-    var players =  [
-      { user : {'name' : 'JPP', 'email' : 'jppampin@gmail.com' , 'isAdmin' : true }, 'confirm': false},
-      { user : {'name' : 'JPP2', 'email' : 'jppampin@gmail.com', 'isAdmin ': false }, 'confirm': false},
-      { user : {'name' : 'JPP3', 'email' : 'jppampin@gmail.com', 'isAdmin' : false }, 'confirm': true},
-      { user : {'name' : 'JPP4', 'email' : 'jppampin@gmail.com', 'isAdmin' : false  }, 'confirm': true}
-    ];
+  function listService($log, $resource, $http) {
+    var players =  [];
+    var matchApi = $resource('/api/match');
 
     var service = {
+      matchId : null,
       players: players,
       addPlayer : addPlayer
     };
 
+    init();
+
     return service;
 
-    function addPlayer(player) {
-      players.push(player);
+    function init(){
+      matchApi.query().$promise.then(function(matches){
+        var match = matches[0];
+        service.matchId = match._id;
+        match.players.forEach(function(player){
+          service.players.push(player);
+        });
+      });
     }
+
+    function addPlayer(player) {
+      var hasPlayer = _.find(players, function(playerListed){
+        return player.user.local.email === playerListed.user.local.email;
+      });
+
+      if(hasPlayer)
+        return;
+
+      return $http({
+        method: 'POST',
+        url: '/api/match/' + service.matchId + '/addPlayer' ,
+        data: player,
+        headers: { 'Content-Type': 'application/json' }
+      }).then(function () {
+        return players.push(player);        
+      });
+
+    }
+
   }
 })();
